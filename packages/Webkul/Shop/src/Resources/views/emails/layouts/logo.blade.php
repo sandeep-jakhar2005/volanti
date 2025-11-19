a4f61ce9a8485577b556e9b320aa6af3228e0d73
Tanish <144316755+Tanish-Wahengbam@users.noreply.github.com>
2025-11-19 17:41:08 +0530

    update password mail logo and checkout popup

--- packages/Webkul/Shop/src/Resources/views/emails/layouts/logo.blade.php
+++ packages/Webkul/Shop/src/Resources/views/emails/layouts/logo.blade.php
@@ -1,5 +1,5 @@
 @if ($logo = core()->getCurrentChannel()->logo_url)
     <img src="{{ $logo }}" alt="{{ config('app.name') }}" style="height: 40px; width: 110px;"/>
 @else
-    <img src="https://images.squarespace-cdn.com/content/v1/6171dbc44e102724f1ce58cf/eda39336-24c7-499b-9336-c9cee87db776/VolantiStickers-11.jpg?format=1500w" alt="{{ config('app.name') }}"/>
+    <img style="width:100%;max-width:300px;display:block;margin:0 auto" src="https://images.squarespace-cdn.com/content/v1/6171dbc44e102724f1ce58cf/eda39336-24c7-499b-9336-c9cee87db776/VolantiStickers-11.jpg?format=1500w"  alt="{{ config('app.name') }}"/>
 @endif
